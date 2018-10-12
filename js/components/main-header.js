e1d8cc643d1c216c8d0816bffd936a50a3c3e016
Marco Barão <marco.barao@outlook.com>
2018-10-12 12:52:53 -0300

    Iniciando o about e resolvendo o header com scroll

--- js/components/main-header.js
+++ js/components/main-header.js
@@ -1,15 +1,29 @@
 'use strict';
 
 (function() {
+    let ticking = false
     const $header = document.querySelector('.main-header')
+    const $hamburger = document.querySelector('.main-hamburger')
     
-    function changeBgc() {
-        if(window.scrollY > 0) {
+    function changeBgc(lksp) {
+        console.log(lksp)
+        if(lksp > 0) {
             $header.classList.add('-background')
+            $hamburger.classList.add('-dark')
         } else {
             $header.classList.remove('-background')
+            $hamburger.classList.remove('-dark')
         }
     }
     
-    window.addEventListener('scroll',  changeBgc)
+    window.addEventListener('scroll',  () => {
+        let last_known_scroll_position = window.scrollY;
+        if (!ticking) {
+            window.requestAnimationFrame(function() {
+                changeBgc(last_known_scroll_position);
+                ticking = false;
+            });
+        }
+        ticking = true;
+    })
 })();
